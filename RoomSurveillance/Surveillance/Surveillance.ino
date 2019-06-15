4808799ae01a4e56fdb63f5eeb9af1fb9745165f
Joshua Schulz <joshua.schulz@sap.com>
2019-06-15 19:11:02 +0200

    RoomSurveillance added lcd display to project

--- RoomSurveillance/Surveillance/Surveillance.ino
+++ RoomSurveillance/Surveillance/Surveillance.ino
@@ -1,38 +1,50 @@
+#include <LiquidCrystal.h>
+
 int ledPin = 13;                // choose the pin for the LED
-int inputPin = 5;               // choose the input pin (for PIR sensor)
+int inputPin = 24;              // choose the input pin (for PIR sensor)
 int pirState = LOW;             // we start, assuming no motion detected
 int val = 0;                    // variable for reading the pin status
- 
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); //In dieser Zeile wird festgelegt, welche Pins des Mikrocontrollerboards für das LCD verwendet wird (Am besten erstmal nicht verändern).
+
 void setup() {
   pinMode(ledPin, OUTPUT);      // declare LED as output
   pinMode(inputPin, INPUT);     // declare sensor as input
  
   Serial.begin(9600);
+  lcd.begin(16, 2);
 
+   lcd.setCursor(0,0);
+   lcd.print("Init");
    while( digitalRead(inputPin) == HIGH){
     Serial.println("Init");
+    lcd.setCursor(0,0);
+    lcd.print("Init");
     delay(1000);
   }
+  lcd.clear();
 }
  
 void loop(){
   val = digitalRead(inputPin);  // read input value
-  if (val == HIGH)  // check if the input is HIGH
-  {            
+  if (val == HIGH) {  // check if the input is HIGH            
     digitalWrite(ledPin, HIGH);  // turn LED ON
   
-    if (pirState == LOW) 
-  {
+    if (pirState == LOW) {
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("Motion detected!");
       Serial.println("Motion detected!"); // print on output change
       pirState = HIGH;
     }
   } 
-  else 
-  {
+  else {
     digitalWrite(ledPin, LOW); // turn LED OFF
   
-    if (pirState == HIGH)
-  {
+    if (pirState == HIGH) {
+      lcd.clear();   
+      lcd.setCursor(0,0);
+      lcd.print("Motion ended!");
       Serial.println("Motion ended!");  // print on output change
       pirState = LOW;
     }
